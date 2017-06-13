39c5a2885dae8eefc1437b747889274c3d32fe0e
Arthur NAEGELY <arthur.naegely@epita.fr>
2017-06-13 23:02:38 +0200

    [FIX] rakuten spider working

--- rakuten/rakuten/spiders/rakuten_spider.py
+++ rakuten/rakuten/spiders/rakuten_spider.py
@@ -12,22 +12,22 @@ class QuotesSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        for bags_info in response.css('div.rsrSResultSect\ clfx'):
+        for bags_info in response.css('div.rsrSResultSect'):
             yield {
                 'url':
                     bags_info.css('div.rsrSResultItemTxt a::attr(href)').extract_first(),
                 'description':
                     bags_info.css('div.rsrSResultItemTxt a::text').extract_first(),
                 'price':
-                    bags_info.css('p.class.price a::text').extract_first(),
+                    bags_info.css('p.price a::text').extract_first(),
                 'picture_url':
-                    bags_info.css('div.rsrSResultPhoto a::attr(src)').extract_first(),
+                    bags_info.css('div.rsrSResultPhoto img::attr(src)').extract_first(),
                 'seller_name':
-                    bags_info.css('div.shopName.span.txtIconShopName a::text').extract_first(),
+                    bags_info.css('span.txtIconShopName a::text').extract_first(),
                 'seller_url':
-                    bags_info.css('div.shopName.span.txtIconShopName a::attr(href)').extract_first(),
+                    bags_info.css('span.txtIconShopName a::attr(href)').extract_first(),
             }
-        next_page = response.css('li.next a::attr(href)').extract_first()
+        next_page = response.css('div.nextPage a::attr(href)').extract_first()
         if next_page is not None:
             next_page = response.urljoin(next_page)
             yield scrapy.Request(next_page, callback=self.parse)
